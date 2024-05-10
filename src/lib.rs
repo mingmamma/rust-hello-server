f50c80e6254fbfcc7ae3754996bc7abbdb04e2bb
Ming Ma <18562714+mingmamma@users.noreply.github.com>
2024-05-10 05:50:41 +0000

    add

--- src/lib.rs
+++ src/lib.rs
@@ -1,152 +1,207 @@
-use std::{thread, sync::{mpsc::{self, Receiver}, Arc, Mutex}};
+// use std::{thread, sync::{mpsc::{self, Receiver}, Arc, Mutex}};
+use std::thread::{self, JoinHandle};
+use std::sync::{Arc, Mutex, mpsc::{channel, Sender, Receiver, RecvError}};
 
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    transmitter: Option<mpsc::Sender<Job>>
+// Hypothetically correct type specification for a value that
+// esscially is a closure of no parameter and returns unit AND 
+// to be passable to other threads for execution. The compiler
+// gives alternative format for actual use with same semantics
+// type Job = Box<dyn FnOnce<(), Output = ()> + Send>;
+
+type Job = Box<dyn FnOnce() -> () + Send>;
+
+struct ThreadWorker {
+    work_thread: Option<JoinHandle<()>>,
 }
 
-// type alias: https://doc.rust-lang.org/book/ch19-04-advanced-types.html?highlight=type%20alias#creating-type-synonyms-with-type-aliases
-type Job = Box<dyn FnOnce() -> () + Send + 'static>;
+impl ThreadWorker {
+    // an exhibit of pitfall that would fail compilation for violating the trait
+    // bounds on parameters of the thread spawn method. Specifically, the
+    // constraint of concern in this case requires the closure arguement to be Send
+    
+    // https://doc.rust-lang.org/std/thread/fn.spawn.html#
+    // pub fn spawn<F, T>(f: F) -> JoinHandle<T>
+    // where F: FnOnce() -> T + Send + 'static, T: ...
+    
+    // On the other hand, in the bogus implementation, the job_receiver is of
+    // type Arc<Receiver<T>> as an argument to the new associated function.
+    // Calling the recv() method in the closure code can be considered as
+    // the closure instance holding (word choice?!) a value of type &Arc<Receiver<T>>,
+    // which is due to the closure's capture by immutable reference of the
+    // outer variable job_receiver of type Arc<Receiver<T>>, to be able to call 
+    // the recv() method on the wrapped value of type Receiver<T> by taking
+    // &Receiver<T>, made accessible by the Deref coersion enabled by Arc<T> implementing
+    // the Deref trait, i.e. &Arc<Receiver<T> -> &Receiver<T> in this case
+    // https://doc.rust-lang.org/std/sync/struct.Arc.html#impl-Deref-for-Arc%3CT,+A%3E
+    // https://doc.rust-lang.org/std/sync/mpsc/struct.Receiver.html#method.recv
+    
+    // Given that Receiver<T> is not Sync, Arc<Receiver<T>> is not Send since Arc<U> is Send
+    // only if U is Sync + Send (https://doc.rust-lang.org/std/sync/struct.Arc.html#impl-Send-for-Arc%3CT,+A%3E)
+    // Given Arc<Receiver<T>> is not Send, &Arc<Receiver<T>> is not Sync, by definition
+    // https://doc.rust-lang.org/std/marker/trait.Sync.html
+    // Hence the closure instance holding a value of type &Receiver<T> is not Send
+    // due to autotrait implementation (elaborate how?! if that is the case), 
+    // contradicting the required trait bound that the closure argument is Send. U+220E
 
-struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Self {
-        let thread = thread::spawn(move || loop {
-            // not sure when the drop of the MutexGuard (by calling .lock().unwrap()) take place in this instance, which releases the lock
-            
-            // Note that lock() call blocks the thread until the lock is acquired
-            
-            // Note that recv() call also blocks the thread if no available data is received, entailing that the thread in the worker 
-            // waits/is blocked until a job is received to be done
-
-            // Note that the exact timing of the drop of MutexGuard (which equates to the release of the acquired lock) can be reasoned as following:
-            
-            // As given by the type hint, the lock() call returns a a MutexGuard smart pointer wrapped in a LockResult
-            // s.t. the following unwrap() call gives the MutexGuard smart pointer. See similar reasoning: https://doc.rust-lang.org/book/ch16-03-shared-state.html#the-api-of-mutext
-            
-            // The MutexGuard smart pointer implements a Deref trait that get used with recv() call with the following reasoning:
-            // As given bby the type hint, since recv() call requires &Receiver type, the MutexGuard<'_, Receiver<...>> returned
-            // by the preceding unwrap() call is Deref coersed into &Receiver<...> to fit the type
-
-            // Coming back to when the timing when the lock is dropped, since the values of the RHS of the let statement is dropped once the statement completes
-            // we conclude that it is at this point, notably before the job runs, that the MutexGuard goes out of the scope
-            // s.t. the Drop trait implementation of the MutexGuard releases the lock
-
-            // See a subtly different behavior as explained in the book if the code is otherwise for the explanation:
-            // while let Ok(job) = receiver.lock().unwrap().recv() {...}
-            // https://doc.rust-lang.org/book/ch20-02-multithreaded.html#implementing-the-execute-method
-            
-            let job_msg = receiver.lock().unwrap().recv();
-
-            match job_msg {
-                Ok(job) => {
-                    println!("Worker {id} got a job executing.");
-                    job();
-                }
-                // given that the infinite loop of recv() call 
-                // the Err arm is taking placing after the transmitter
-                // is dropped
-                Err(_) => {
-                    println!("Worker {id} disconnected; shutting down.");
-                    break;
+    // pub fn new(job_receiver: Arc<Receiver<()>>) -> Self {
+    //     let handle = thread::spawn(|| {
+    //         loop {
+    //             println!("thread worker is just cruising like i am");
+                
+    //             if let Ok(_) = job_receiver.recv() {
+    //                 println!("now i got job to do")
+    //             } 
+    //         }
+    //     });
+        
+    //     Self {
+    //         work_thread: handle,
+    //     }
+    // }
+    
+    pub fn new(job_receiver: Arc<Mutex<Receiver<Job>>>) -> Self {
+        let handle = thread::spawn(move || {
+            loop {
+                // println!("thread worker is just cruising like i am");
+                // an exhibit of a subtly problematic implementation of
+                // acquiring the Mutex lock to receiver to receive a job
+                // for execution. The performance issue with the code
+                // is that the acquired lock guard is NOT dropped until
+                // the job (essentially the passed in closure for execution)
+                // completes running. It becomes a risk if the job is long-running,
+                // preventing other potentially available threads to acquire lock
+                // and obtain other jobs for execution in the meantime
+                // if let Ok(job) = job_receiver.lock().unwrap().recv() {
+                //     println!("now i got job to do");
+                //     job();
+                // }
+                
+                let job_msg: Result<Job, RecvError> = job_receiver.lock().unwrap().recv();
+                match job_msg {
+                    Ok(job) => {
+                        job();
+                    },
+                    // the only circumstance that the recv() method returns Err variant
+                    // is due to dropped sender, hence the workflow of the Drop implementation
+                    // works by first dropping the sender, resulting in breaking out of the
+                    // loop and completing the closure as parameter of the spawn() method,
+                    // and lastly making the join() method call on the thread handle to return
+                    Err(_) => {
+                        break;
+                    }
                 }
             }
         });
         
         Self {
-            id,
-            thread: Some(thread),
-        }
+            work_thread: Some(handle),
+        }            
     }
 }
 
-#[derive(Debug)]
-pub enum PoolCreationError {
-    ZeroThreadCreationError,
+pub struct ThreadPool {
+    job_sender: Option<Sender<Job>>,
+    thread_workers: Vec<ThreadWorker>,
 }
 
 impl ThreadPool {
-    /// Create a new ThreadPool.
-    /// The size is the number of threads in the pool.
-    pub fn build(num: usize) -> Result<ThreadPool, PoolCreationError> {
+    pub fn build(thread_num: usize) -> Self
+    {   
+        // simple idiomatic prerequisite validation
+        assert_ne!(thread_num, 0);
         
-        if num == 0 {
-            return Err(PoolCreationError::ZeroThreadCreationError);
-        }
-
-        // doing (known) vector space allocation up front is slightly more efficient than using Vec::new
-        let mut workers = Vec::with_capacity(num);
+        let (job_sender, job_receiver) = channel::<Job>();
         
-        // Note that the sender is clonable s.t. they can be
-        // moved into multiple spawned threads to send messages
-        // simultaneously, see: https://doc.rust-lang.org/rust-by-example/std_misc/channels.html
-        // The current use case IS NOT the above
-        let (sender, receiver) = mpsc::channel();
-
-
-        // Note that receiver can't be cloned and must be owned by one thread: https://doc.rust-lang.org/std/sync/mpsc/struct.Receiver.html
-        // As such, here we'd like to share the ownership of ONE receiver among multiple threads
-        // The atomic reference count (ARC) paired with mutual exclusion is a common solution pattern: https://doc.rust-lang.org/std/sync/mpsc/struct.Receiver.html
-        // Arc<T> requires type T to be a Sync type s.t. references like &T are safe to share across threads
-        // As such, since Mutex is a Sync type, Arc<Mutex> enables thread-safe interior mutability for ONE shared ownership
-        let receiver = Arc::new(Mutex::new(receiver));
-
-        for i in 0..num {
-            let receiver = Arc::clone(&receiver);
-            workers.push(Worker::new(i, receiver));
+        let mut thread_workers: Vec<ThreadWorker> = Vec::with_capacity(thread_num);
+        
+        let job_receiver = Arc::new(Mutex::new(job_receiver));
+        for _ in 0..thread_num {
+            let job_receiver_copy: Arc<Mutex<Receiver<Job>>> = Arc::clone(&job_receiver);
+            let new_thread_worker = ThreadWorker::new(job_receiver_copy);
+            thread_workers.push(new_thread_worker);
         }
-
-        Ok(ThreadPool {workers, transmitter: Some(sender)})    
+        
+        Self {
+            job_sender: Some(job_sender),
+            thread_workers: thread_workers,
+        }
+        
     }
-
-    pub fn execute<F>(&self, f: F)
-    where
-        // An instance of using the trait bound
-        // https://doc.rust-lang.org/book/ch10-02-traits.html?highlight=trait%20bound#using-trait-bounds-to-conditionally-implement-methods
-        F: FnOnce() -> () + Send + 'static
-    {
+    
+    // an example of incorrect signature design using Fn trait. Noting the
+    // call site use, we see that the passed in closure arguement captures
+    // an outer variable of type TcpStream, which is not copy, and hence would
+    // involve taking the ownership of the captured variable by the effect of
+    // the catpture. Such behaviour contradicts with the constraint of Fn trait,
+    // whose class of closures can only capture outer variables by immutable
+    // reference or not capturing any out variable at all
+    // pub fn execute<F>(&self, f: F) -> () where F: Fn() -> ()
+    
+    // for what reason does the type parameter need to be explicitly static?!
+    pub fn execute<F>(&self, f: F) -> () where F: FnOnce() -> () + Send + 'static
+    {   
         let job = Box::new(f);
-        
-        // Use as_ref() to obtain an Option of the reference to the sender inside the
-        // option, which is ready to be consumed with unwrap().send()...
-        // This manuveur leaves the transmitter Option untouched which is required since
-        // it is a field in the ThreadPool struct while still gaining access to the sender
-        self.transmitter.as_ref().unwrap().send(job).unwrap();
+        self.job_sender.as_ref().unwrap().send(job).expect("failed to send job to worker thread for execution");
     }
 }
 
+// best-effort bogus Drop implementation fails due to calling JoinHandle<T>'s join() method
+// on a JoinHandle<T> value being a field of the ThreadWorker struct. It violates borrow 
+// borrow checking since at the point of the call, only a shared reference to the ThreadWorker
+// instance is available. The Option wrapping can work around to move a field value out
+// to allow the join() method call to take ownership and come through
+// impl Drop for ThreadPool {
+//     fn drop(&mut self) {
+//         for thread_worker_ref in &self.thread_workers {
+//             thread_worker_ref.work_thread.join().expect("failed to join work thread handle");
+//         }
+//     }
+// }
+
 impl Drop for ThreadPool {
     fn drop(&mut self) {
-        
-        // Dropping the transmiter as the first part of the drop implementation of ThreadPool
-        // s.t. the infinite loop in the spawned thread containing the receiver enter the Err
-        // the break path s.t. they are deemed completed when join() is later called on the thread
-        if let Some(transmitter) = self.transmitter.take() {
-            core::mem::drop(transmitter);
+        drop(self.job_sender.take().unwrap());        
+    
+        for thread_worker_ref_mut in &mut self.thread_workers {
+            let work_thread_handle = thread_worker_ref_mut.work_thread.take().unwrap();
+            work_thread_handle.join().expect("failed to join worker thread");
         }
+    }
+}
 
-        // use pattern matching of for loop to fine tune
-        // the binding spec of the worker variable/pattern after for
-        // https://doc.rust-lang.org/book/ch18-01-all-the-places-for-patterns.html#for-loops
-        for worker in &mut self.workers {
-
-            println!("Shutting down worker {}", worker.id);
-
-            // This manuveur is required since the thread is a field of the worker struct
-            // thread.join() consumes the ownership of the thread, which is impossible if
-            // thread is a direct field of a worker struct. Instead, by wrapping the thread
-            // with an option, we gain ownership of the thread by using take() on the option
-            // s.t. thread ownership is obtained and None is left inside the worker
-            // https://doc.rust-lang.org/book/ch20-03-graceful-shutdown-and-cleanup.html#implementing-the-drop-trait-on-threadpool
-            if let Some(thread) = worker.thread.take() {
-                // join blocks the current (main) thread to wait for the associated thread to finish
-                // https://doc.rust-lang.org/std/thread/struct.JoinHandle.html#method.join
-                thread.join().unwrap();
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    use rand::{self, Rng};
+    use std::time::Duration;
+    
+    #[test]
+    fn thread_pool_integration() {
+        let mut test_counter = Arc::new(Mutex::new(0));
+        {
+            let test_thread_pool = ThreadPool::build(4);
+            for _ in 0..5 {
+                let test_counter_clone = Arc::clone(&test_counter);
+                test_thread_pool.execute(move || {
+                    let rnd_pause: u64 = rand::thread_rng().gen_range(999..1000);
+                    thread::sleep(Duration::from_millis(rnd_pause));
+                    
+                    let mut counter_guard = test_counter_clone.lock().expect("failed to acquired Mutex lock");
+                    *counter_guard = *counter_guard + 1;
+                });            
             }
         }
+    
+        // hack measure to sleep to main thread to make sure the spawned worker threads in the thread pool
+        // gets sufficient time to execute the inteeded jobs and not terminated prematurely as a result of the
+        // completion of the main thread. It becomes unneccessary with properly implemented graceful shutdown
+        // on ThreadPool, such that it is guaranteed that worker threads join after the completion on in-flight
+        // jobs as part of the Drop implementation getting executed on the main thread when the ThreadPool instance 
+        // goes out of scope, thus making sure that code after the scope of the ThreadPool "happens-after"
+        // the all the spawned jobs in worker threads are completed
+        // thread::sleep(Duration::from_secs(1));
+        
+        assert_eq!(*test_counter.lock().expect("failed to acquired Mutex lock"), 5);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead, Write}, fs, thread, time::Duration};
+use std::net::{TcpListener, TcpStream};
+use std::io::{self, BufReader, BufRead, Write};
+use std::fs;
+use std::thread;
+use std::time::Duration;
 use hello_rust_server::ThreadPool;
-
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::build(4).expect("Server setup error");
+    let pool = ThreadPool::build(4);
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
+    for stream_result in listener.incoming() {
+        let stream = stream_result.unwrap();
         pool.execute(|| {
-            handle_connection(stream);
+            // respond_by_request_line(stream);
+            respond_by_request_verbatim_print(stream);
         })
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn respond_generic_page_by_request_line(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     
     // Collecting the http request into a vector and print the request info 
@@ -45,4 +48,25 @@ fn handle_connection(mut stream: TcpStream) {
 
 
     stream.write_all(response.as_bytes()).unwrap();                    
+}
+
+fn respond_by_request_verbatim_print(mut stream: TcpStream) {
+    let buf_reader: BufReader<&mut TcpStream> = BufReader::new(&mut stream);
+    
+    // desugared & annotated equivalent implementation
+    // let lines: Lines<BufReader<&mut TcpStream>> = buf_reader
+    //             .lines();
+    // let mapped_lines = lines.map(|line: Result<String, Error>| -> String 
+    //     { line.unwrap() });
+    // let http_request: Vec<String> = mapped_lines
+    //                                 .take_while(|line| !line.is_empty())
+    //                                 .collect();
+    
+    let http_request: Vec<String> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
